8d291659ea12b9ead869c1e9c9da71079037ed49
Aaron <aw9994@cs.ship.edu>
2019-11-13 17:59:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# brewery-browser
+ A brewery browser website for the Harrisburg, PA area.
---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Harrisburg Brewery Browser
+        <BreweryComponent></BreweryComponent>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+class BreweryComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.openbrewerydb.org/breweries?by_city=harrisburg")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <table class="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Type</th>
+              <th scope="col">Address</th>
+              <th scope="col">Website</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map(item => (
+              <tr>
+                <td>{item.name}</td>
+                <td>{item.brewery_type}</td>
+                <td>{item.street}</td>
+                <td><a href={item.website_url}>{item.website_url}</a></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    }
+  }
+}
